fee6990ca1202ae1a07bcaee78b8104ec6ee166c
hritik <hritikdrocks@gmail.com>
2017-08-31 12:56:25 +0000

    Added support for GCC 6.0

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Aug 31 01:32:44 UTC 2017"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "hritik-harpia-kernel-5300583"
+#define LINUX_COMPILER "gcc version 6.0.0 20160401 (experimental) (UBERTC-6.0.0) "
